028a062663b792f0d869e35cd82223ef4a5d7161
leticiasmx <sammyaleticia1234@gmail.com>
2025-10-25 11:26:51 -0300

    Add NodeSimples class for linked list implementation

---
+++ NodeSimples.java
@@ -0,0 +1,11 @@
+package ListaSimples;
+
+public class NodeSimples {
+	 public int data;
+	    public Node next;
+	    
+	    public Node(int data) {
+	        this.data = data;
+	        this.next = null;
+	    }
+}
